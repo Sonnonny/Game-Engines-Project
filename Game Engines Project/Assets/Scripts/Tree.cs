1b5657ed579281049ba0e9d0db5117bbe4a7ad16
Sonnonny <61233430+Sonnonny@users.noreply.github.com>
2020-12-22 15:59:55 +0000

    Starting Over

---
+++ Game Engines Project/Assets/Scripts/Tree.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+
+    public GameObject[] parts;
+
+    public int iterations;
+    public Vector2 minMax;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iterations = (int)Random.Range(minMax.x, minMax.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
