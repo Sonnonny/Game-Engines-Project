24dc5631e48bbd7103915b13252eed5ceaeee9f5
Sonnonny <61233430+Sonnonny@users.noreply.github.com>
2020-12-24 14:35:47 +0000

    music

--- Game Engines Project/Assets/Scripts/PlayerController.cs
+++ Game Engines Project/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,9 @@ public class PlayerController : MonoBehaviour
     Vector3 moveDirection;
     public Rigidbody rb;
     public float moveSpeed;
+    public float runMult;
     public LayerMask groundLayer;
+    public float jumpPower;
 
     // Start is called before the first frame update
     void Start()
@@ -22,18 +24,23 @@ public class PlayerController : MonoBehaviour
         moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
 
 
-        Vector3 jump = new Vector3(0, 0, 0);
+        //Vector3 jump = new Vector3(0, 0, 0);
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Physics.Raycast(transform.position, -transform.up, 2,groundLayer);
 
 
-
-            jump = new Vector3(0, 8, 0);
+            rb.AddForce(transform.up *jumpPower, ForceMode.Impulse);
+            //jump = new Vector3(0, 8, 0);
         }
 
         //rb.velocity += (moveDirection+jump )* moveSpeed * Time.deltaTime;
+        float run = 1;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            run = runMult;
+        }
 
-        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection + jump) * moveSpeed * Time.deltaTime);
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection) * moveSpeed * run* Time.deltaTime);
     }
 }
--- Game Engines Project/Assets/Scripts/Tree.cs
+++ Game Engines Project/Assets/Scripts/Tree.cs
@@ -14,6 +14,8 @@ public class Tree : MonoBehaviour
 
     List<GameObject> others;
 
+    public bool react;
+
     //public WorldGravity worldGravity;
 
     // Start is called before the first frame update
@@ -54,6 +56,30 @@ public class Tree : MonoBehaviour
         iterations--;
         GameObject Go = Instantiate(parts[Random.Range(0,parts.Length)],transform.position,Quaternion.identity,transform);
         Go.GetComponent<Part>().Create(iterations,iterations+1);
+    }
+
+
+    private void Update()
+    {
+        if (!react) return;
+        float[] spectrum = new float[256];
+
+        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+
+        for (int i = 1; i < spectrum.Length - 1; i++)
+        {
+            Debug.Log(spectrum[i]);
+
+            //transform.localScale = new Vector3(500*spectrum[i],500*spectrum[i],500*spectrum[i]);
+
+            //Debug.Log("help");
+
+            //Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
+            //Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
+            //Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
+            //Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
+        }
+        
     }
 
 }
