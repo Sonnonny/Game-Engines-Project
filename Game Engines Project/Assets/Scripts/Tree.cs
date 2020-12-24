32c94a95b28e27044c3d3fc2b2411f4cfe8ccc8f
Sonnonny <61233430+Sonnonny@users.noreply.github.com>
2020-12-24 14:59:31 +0000

    optimisations

--- Game Engines Project/Assets/Scripts/Tree.cs
+++ Game Engines Project/Assets/Scripts/Tree.cs
@@ -14,7 +14,9 @@ public class Tree : MonoBehaviour
 
     List<GameObject> others;
 
-    public bool react;
+    public bool master;
+
+    static float sizeMult;
 
     //public WorldGravity worldGravity;
 
@@ -61,25 +63,21 @@ public class Tree : MonoBehaviour
 
     private void Update()
     {
-        if (!react) return;
-        float[] spectrum = new float[256];
-
-        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
-
-        for (int i = 1; i < spectrum.Length - 1; i++)
+        if (master) 
         {
-            Debug.Log(spectrum[i]);
+            float[] spectrum = new float[256];
 
-            //transform.localScale = new Vector3(500*spectrum[i],500*spectrum[i],500*spectrum[i]);
+            AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
 
-            //Debug.Log("help");
+            for (int i = 1; i < spectrum.Length - 1; i++)
+            {
+                sizeMult = 1 + spectrum[i] * 500;
 
-            //Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
-            //Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
-            //Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
-            //Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
+            }
         }
-        
+
+        transform.localScale = new Vector3(sizeMult, sizeMult, sizeMult);
+
     }
 
 }
